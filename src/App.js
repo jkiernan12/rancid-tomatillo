b66a5cd76d020839c1a6c518d9787bc2f0846f3c
John Kiernan <jkiernan12@yahoo.com>
2021-12-02 17:34:31 -0500

    Revert "Merge pull request #5 from jkiernan12/feat/displaying-movies"
    This reverts commit a70489631c6c76acdb60f942c86cd00b3b569e41, reversing
    changes made to f892751bc41eedf93cc059a1ef4431d121864122.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import MovieContainer from './MovieContainer';
+import logo from './logo.svg';
 import './App.css';
-import movieData from './MovieData';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      movies: movieData.movies
-    }
-  }
-
-  render() {
-    return (
-      <main className='App'>
-        <h1>Movies</h1>
-        <MovieContainer movies={this.state.movies}/>
-      </main>
-    )
-  }
-
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
