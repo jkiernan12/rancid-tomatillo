4ad43ef5f8576f951f9e23348fc4ce36c0fc1b09
Adam Burgess <aburg1515@gmail.com>
2021-12-02 16:06:48 -0700

    Second attempt at push

--- src/Poster.js
+++ src/Poster.js
@@ -5,7 +5,6 @@ const Poster = ({id, poster_path, backdrop_path, title, average_rating, release_
     <div className='poster'>
       <h1>{title}</h1>
       <p>{average_rating}</p>
-    
     </div>
 
 
