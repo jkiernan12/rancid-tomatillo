167fdd5e02b87e3aa626bcdaaca83a5153c76491
John Kiernan <jkiernan12@yahoo.com>
2021-12-13 15:24:22 -0500

    Fix linting errors

--- src/fetch.js
+++ src/fetch.js
@@ -7,10 +7,10 @@ export const allMovieData = () => {
         throw res.message
       }
     })
-  }
+}
 
 export const singleMovieData = (id) => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/'+ id)
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/' + id)
     .then(res => {
       if (res.ok) {
         return res.json()
